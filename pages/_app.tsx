10f472c6a74a8540b7d14b8d255e9df0904e174e
Jeff Wen <sinchangwen@gmail.com>
2023-03-13 10:42:04 +0000

    update umami id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script
-        data-website-id='ffede72a-c791-46bd-ab88-1c5f52d6dfa6'
+        data-website-id='7f340b54-8669-43af-8946-65d0148546b5'
         src='https://umami-production-af5b.up.railway.app/umami.js'
         strategy='afterInteractive'
       />
